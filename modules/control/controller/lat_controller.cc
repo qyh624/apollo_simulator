abe40a3eaf238175100df81cbd0bb49021da10cd
qyh624 <qyh13570@gmail.com>
2024-07-11 15:17:18 +0800

    Update lat_controller.cc

--- modules/control/controller/lat_controller.cc
+++ modules/control/controller/lat_controller.cc
@@ -483,11 +483,11 @@ Status LatController::ComputeControlCommand(
         matrix_q_(2, 2) * heading_err_interpolation_->Interpolate(
                               std::fabs(vehicle_state->linear_velocity()));
     common::math::SolveLQRProblem(matrix_adc_, matrix_bdc_, matrix_q_,
-                                  matrix_r, lqr_eps_, lqr_max_iteration,
+                                  matrix_r, lqr_eps_, lqr_max_iteration_,
                                   &matrix_k_);
   } else {
     common::math::SolveLQRProblem(matrix_adc_, matrix_bdc_, matrix_q_,
-                                  matrix_r, lqr_eps_, lqr_max_iteration,
+                                  matrix_r, lqr_eps_, lqr_max_iteration_,
                                   &matrix_k_);
   }
 
